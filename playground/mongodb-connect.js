f56c419f0096316beeba53a6fca282502f2b8fd0
kanhery <kanhery>
2017-06-23 22:18:49 +0530

    Init Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,31 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db) => {
+    if(err) {
+        return console.log("Error Connecting to MongoDB server");
+    }
+    console.log("Connected to MongoDB server");
+
+    // db.collection('Todos').insertOne({
+    //     text : "Something to do more",
+    //     completed : false
+    // },(err, result) => {
+    //     if (err) {
+    //         return console.log('Error while inserting to Todos',err)
+    //     }
+    //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp()));
+    // });
+
+    db.collection('Users').insertOne({
+        name : "kanhery",
+        age : 34,
+        location : "Indore"
+    },(err, result) => {
+        if (err) {
+            return console.log('Error while inserting to Todos',err)
+        }
+        console.log(JSON.stringify(result.ops[0]._id.getTimestamp()));
+    });
+
+    db.close();
+})
